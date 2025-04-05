ed6f9fb50904c5f81752770fa24571783b053d96
jason2000-cpu <jasonmwangi2000@gmail.com>
2025-04-05 22:45:54 +0300

    update: freshly re-wrote the service worker for full offline experience

--- public/sw.js
+++ public/sw.js
@@ -1,102 +1,46 @@
-
-importScripts(
-    'https://storage.googleapis.com/workbox-cdn/releases/7.3.0/workbox-sw.js'
-);
-
-// This is your Service Worker, you can put any of your custom Service Worker
-// code in this file, above the `precacheAndRoute` line.
-
-// perform caching on install
-
-const cacheName = "static-shell-v1";
-const resourcesToPrecache = [
+const CACHE_NAME = 'mydoctor-cache-v1';
+const urlsToCache = [
     '/',
     '/index.html',
-    '/widget/',
-    '/assets/',
-    '/.well-known/'
-];
+    'manifext.json',
+    '/icon-192x192.png',
+    'icon-512x512.png'
+]
 
+
+// Install event
 self.addEventListener('install', event => {
-    console.log('Service worker install event!');
-    event.waitUntil(
-        caches.open(cacheName)
-        .then(cache => {
-            return cache.addAll(resourcesToPrecache)
-        })
-        .catch(error => {
-            console.log('[Service Worker] Pre-caching failed.', error)
-        })
+    console.log('[sw] Install');
+    event.waitUntill(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(urlsToCache))
     )
-});
+})
 
+// Activate Event
 self.addEventListener('activate', event => {
-    console.log('[Service Worker] Activate event');
-    event.waitUntil(
-        caches.keys().then(cacheNames => {
-            return Promise.all(
-                cacheNames.map(cache => {
-                    if(cache != cacheName) {
-                        console.log('[Service Worker] Deleting old cache::', cache);
-                        return caches.delete(cache);
+    console.log('[SW] Activate');
+    event.waitUntill(
+        caches.keys().then(keyList => 
+            Promise.all(
+                keyList.map(key => {
+                    if (key !== CACHE_NAME) {
+                        console.log('[SW] Deleting old cache:', key);
+                        return caches.delete(key)
                     }
                 })
             )
-        })
+        )
     );
-    self.clients.claim();
+    return self.clients.claim();
+});
+
+
+// Fetch
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(response => 
+            response || fetch(event.request)
+        )
+    );
 })
-
-// When widget is installed/pinned, push initial state.
-self.addEventListener('widgetinstall', (event) => {
-    event.waitUntil(updateWidget(event));
-});
-
-// When widget is shown, update content to ensure it is up-to-date.
-self.addEventListener('widgetresume', (event) => {
-    event.waitUntil(updateWidget(event));
-});
-
-// When the user clicks an element with an associated Action.Execute,
-// handle according to the 'verb' in event.action.
-self.addEventListener('widgetclick', (event) => {
-if (event.action == "updateName") {
-    event.waitUntil(updateName(event));
-}
-});
-
-// When the widget is uninstalled/unpinned, clean up any unnecessary
-// periodic sync or widget-related state.
-self.addEventListener('widgetuninstall', (event) => {});
-
-const updateWidget = async (event) => {
-// The widget definition represents the fields specified in the manifest.
-    const widgetDefinition = event.widget.definition;
-
-    // Fetch the template and data defined in the manifest to generate the payload.
-    const payload = {
-        template: JSON.stringify(await (await fetch(widgetDefinition.msAcTemplate)).json()),
-        data: JSON.stringify(await (await fetch(widgetDefinition.data)).json()),
-    };
-
-    // Push payload to widget.
-    await self.widgets.updateByInstanceId(event.instanceId, payload);
-}
-
-const updateName = async (event) => {
-    const name = event.data.json().name;
-
-    // The widget definition represents the fields specified in the manifest.
-    const widgetDefinition = event.widget.definition;
-
-    // Fetch the template and data defined in the manifest to generate the payload.
-    const payload = {
-        template: JSON.stringify(await (await fetch(widgetDefinition.msAcTemplate)).json()),
-        data: JSON.stringify({name}),
-    };
-
-    // Push payload to widget.
-    await self.widgets.updateByInstanceId(event.instanceId, payload);
-}
-
-workbox.precaching.precacheAndRoute(self.__WB_MANIFEST || []);
